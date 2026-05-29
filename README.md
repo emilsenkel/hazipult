996eb3fb0652908d0f0da751bcd5cdccf0d5da51
emilsenkel <emil.senkel@gmail.com>
2026-05-30 00:39:40 +0200

    Initial HáziPult PWA setup - manifest, service worker, vercel config

---
+++ README.md
@@ -0,0 +1,5 @@
+# 📚 HáziPult
+
+**Személyes házi feladat szervező 13 éveseknek**
+
+... (see full README in the folder)
---
+++ sw.js
@@ -0,0 +1,48 @@
+// HáziPult Service Worker - Offline support
+const CACHE_NAME = 'hazipult-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(() => {});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin === self.location.origin) {
+    event.respondWith(
+      caches.match(event.request).then((cached) => {
+        return cached || fetch(event.request).then((response) => {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+          return response;
+        });
+      })
+    );
+    return;
+  }
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
